817772f6421acb3ff6cb7a0c98b8d92c417c566d
Tanmay <tanmaygautam1797@gmail.com>
2024-07-31 12:34:45 +0530

    previous api got discontinued so added a new api

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export async function productsData() {
   const products = await axios.get(
-    "https://fakestoreapiserver.vercel.app/amazonproducts"
+    // "https://fakestoreapiserver.vercel.app/amazonproducts"
+    "https://fakestoreapi.com/products"
   );
+  console.log("pr", products);
   return products;
 }
